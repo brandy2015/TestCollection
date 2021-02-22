7da664639fa88757c2830b5e341425df1bece6c4
XYZ <10484324+brandy2015@users.noreply.github.com>
2021-02-22 08:17:52 +0800

    更新

---
+++ TestCollection/TestUICollectionViewVC.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  TestCollection
+//
+//  Created by zhangzihao on 2021/2/21.
+//
+
+import UIKit
+
+class TestUICollectionViewVC: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    var dataSource = [
+        ["name":"地址管理","pic":"Birthdays"],
+        ["name":"我的收藏","pic":"Chores"],
+        ["name":"账号信息","pic":"Drinks"],
+        ["name":"系统设置","pic":"Folder"]
+    ]
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        dataSource = dataSource + dataSource + dataSource
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCellx", for: indexPath) as! TestcollectionViewCell
+        let data = dataSource[indexPath.row]
+        
+        cell.imageView.image = UIImage(named: data["pic"] ?? "")
+        cell.titleLabel.text = data["name"] ?? ""
+        return cell
+    }
+}
---
+++ TestCollection/TestcollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  collectionViewCell.swift
+//  TestCollection
+//
+//  Created by zhangzihao on 2021/2/22.
+//
+
+import UIKit
+
+class TestcollectionViewCell: UICollectionViewCell{
+     
+     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
